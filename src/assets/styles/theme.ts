eddcad387fcf9cd5827c241f1a71e5a659ed29d3
MOON <muneon96@naver.com>
2021-07-27 18:46:36 +0900

    styles폴더 생성

---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,11 @@
+ const lightMode = {
+    bgColor: "#F8F7F4",
+    textColor: "#31302E",
+}
+
+const darkMode = {
+    bgColor: "#1E1E22",
+    textColor: "#ffffff",
+};
+  
+export { lightMode, darkMode };
