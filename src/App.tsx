6c828daffdf0a6f0094e44d225807a5480b451e8
MOON <muneon96@naver.com>
2021-07-31 15:08:28 +0900

    다크모드 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./assets/styles/GlobalStyle";
+import { useDarkMode } from "./assets/hooks/useDarkMode";
+import { dark, light } from "./assets/styles/theme";
+import Toggle from "./components/Toggle";
 
 function App() {
-  return <div className="App"></div>;
+  const [themeMode, toggleTheme] = useDarkMode();
+  const theme = themeMode === "light" ? { mode: light } : { mode: dark };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Container>
+        <h1>darkMode-ts</h1>
+        <Toggle themeMode={themeMode} toggleTheme={toggleTheme} />
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
+
+const Container = styled.div`
+  background-color: ${({ theme }) => theme.mode.mainBackground};
+  color: ${({theme}) => theme.mode.text};
+`;
